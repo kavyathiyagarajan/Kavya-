3469b16f7e964e004e3d6b1d81818e5a9985d364
kavyathiyagarajan <kavyathiyagarajan16@gmail.com>
2026-06-07 15:42:17 +0530

    useEffect and router-dom compleated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,26 @@
 import Counter from './components/Counter'
+import Effect from './components/Effect'
 import Navbar from './components/Navbar'
 import Parent from './components/Parent'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
 import Service from './pages/Service'
+import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    //Fragement tag
     <>
     <Navbar/>
-    <h1>Hello world!!</h1>
-    <h1>Hello world!!</h1>
-    <Parent/>
-    <Home/>
-    <About/>
-    <Contact/>
-    <Service/>
-    <Counter/>
+    <Routes>
+      <Route path='/home' element ={<Home/>}/>
+      <Route path='/about' element ={<About/>}></Route>
+      <Route path='/contact' element ={<Contact/>}></Route>
+      <Route path='/service' element ={<Service/>}></Route>
+      <Route path='/parent' element ={<Parent/>}></Route>
+      <Route path='/useState' element ={<Counter/>}></Route>
+      <Route path='/useEffect' element ={<Effect/>}></Route>
+      </Routes>
 
     </>
   )
--- frontend/src/components/Counter.jsx
+++ frontend/src/components/Counter.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 
-const Counter = () => {
+const Counter = () => 
+  {
      const [count,setCount] = useState(0);
-    
      const handleIncrement = () =>{setCount(count+1)}
      const handleDecrement = () =>{setCount(count-1)}
-     const handleReset = () =>{setCount(count-1)}
+     const handleReset = () =>{setCount(0)}
   return (
     <div>
         <h1>{count}</h1>
         <button onClick={handleIncrement}>Increment</button>
-        <button onClick={handleDecrement}>Reset</button>
-        <button onClick={handleReset}>Decrement</button>
+        <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={handleReset}>Reset</button>
 
     </div>
   )
